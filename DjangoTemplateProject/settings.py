de47bf7058bf203e1d666fb7ba30dadcbe5b4afb
milan.ashara <milan.ashara@v-key.com>
2016-09-30 13:34:59 +0800

    Models

--- DjangoTemplateProject/settings.py
+++ DjangoTemplateProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myapp.apps.myappConfig',
     'axes',
+    'django_countries',
 ]
 
 MIDDLEWARE_CLASSES = [
--- myapp/models.py
+++ myapp/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
 from django.contrib.gis.db import models
+from djmoney.models.fields import MoneyField
+from django_countries.fields import CountryField
 
 class ip_range(models.Model):
     pub_date = models.DateTimeField('date published')
@@ -11,10 +13,22 @@ class ip_range(models.Model):
     
 
 class Restaurant(models.Model):
-#     name = models.CharField(max_length=200)
-    logo = models.ImageField('Company Logo')
-    name = models.CharField(max_length=100)
+    logo = models.ImageField('Restaurant Logo')
+    name = models.CharField('Restaurant Name',max_length=100)
+    address = models.CharField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    country = CountryField()
+
+class MenuGroup(models.Model):
+    name = models.CharField(max_length=100) 
+    restuarant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    
+class Menu(models.Model):
+    item_name = models.CharField('Item Name',max_length=100)
+    item_image = models.ImageField('Item Image')
+    description = models.CharField(max_length=100)
+    price = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+    menu_group = models.ForeignKey(MenuGroup, on_delete=models.CASCADE)
     
   
 
