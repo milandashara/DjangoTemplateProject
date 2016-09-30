d9f6a4a6c073ad739bea245f7fd35178eebeacb1
milan.ashara <milan.ashara@v-key.com>
2016-09-30 13:48:37 +0800

    model

--- myapp/models.py
+++ myapp/models.py
@@ -18,33 +18,32 @@ class Restaurant(models.Model):
     address = models.CharField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     country = CountryField()
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return u"%s" % self.name
 
 class MenuGroup(models.Model):
     name = models.CharField(max_length=100) 
     restuarant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return u"%s" % self.name
     
 class Menu(models.Model):
     item_name = models.CharField('Item Name',max_length=100)
     item_image = models.ImageField('Item Image')
     description = models.CharField(max_length=100)
-    price = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+    price = MoneyField(max_digits=10, decimal_places=2, default_currency='INR')
     menu_group = models.ForeignKey(MenuGroup, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.item_name
+
+    def __unicode__(self):
+        return u"%s" % self.item_name
     
-  
 
-
-
-# class Zipcode(models.Model):
-#     code = models.CharField(max_length=5)
-#     poly = models.PolygonField()
-# 
-# 
-# 
-# class Address(models.Model):
-#     num = models.IntegerField()
-#     street = models.CharField(max_length=100)
-#     city = models.CharField(max_length=100)
-#     state = models.CharField(max_length=2)
-#     zipcode = models.ForeignKey(Zipcode, on_delete=models.CASCADE)
-#     objects = models.GeoManager()
     
